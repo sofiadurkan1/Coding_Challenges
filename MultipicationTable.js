762c871446fa8856fad0adfa9d873eb451635597
sofia durkan <sofiadurkan@gmail.com>
2021-07-02 12:55:00 -0500

    Multipication table solution added.

---
+++ MultipicationTable.js
@@ -0,0 +1,29 @@
+// Create a multipication table 
+
+//Solution 1:
+
+const number = parseInt(prompt('Please enter number'))
+for(let i = 1; i<=10; i++){
+   let result =i * number;
+   console.log(`${i} * ${number} = ${result}`);
+
+
+}
+
+
+// Solution 2:
+
+// for (let i = 1; i<=10;i++){
+//     document.write("<br/>")
+//     for(let j = 1; j<=10; j++){
+
+//     document.write(`${i}*${j}=${i*j}`,"<br/>");
+
+
+        
+//     }
+
+// }
+
+
+
