b6d1ad51292309034fe7ee01f2818f0d9b0c504c
sofia durkan <sofiadurkan@gmail.com>
2021-07-06 08:35:23 -0500

    first commit

---
+++ joinArrayIntoString.js
@@ -0,0 +1,9 @@
+//Question 
+// join all elements of the following array into a string. 
+
+// myColor = ["Red", "Green", "White", "Black"];
+// console.log(myColor.toString());
+// console.log(myColor.join("+"));
+
+
+
