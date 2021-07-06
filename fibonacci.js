7d8cadb1e2e9ea329538ec2641501f15b7bee19b
sofia durkan <sofiadurkan@gmail.com>
2021-07-06 15:34:20 -0500

    second solution added

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,6 @@
+
+//first solution 
+
 var a,b;
 a = 0;
 b = 1;
@@ -8,3 +11,23 @@ for (i = 1;i<50;i++){
     a =b;
     b = result;
 }
+
+//second solution
+
+function fibonacci(num){
+    let x = 0;
+    let y = 1;
+    let result;
+
+    while (num>0){
+        result = x +y;
+        x = y;
+        y = result;
+        num = num - 1;
+        console.log(result);
+
+
+    }
+
+}
+fibonacci(5);
