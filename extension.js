402f6fbb3eb05cb21ad5536e74a989aad7c802d5
sofia durkan <sofiadurkan@gmail.com>
2021-07-18 22:08:24 -0500

    solution added

--- extension.js
+++ extension.js
@@ -1,2 +1,19 @@
 //Write a Js program to get the extension of a filename...
-const getExtension = (str) => str.slice()
+
+//first solution
+
+// let fileName = 'hello.txt'
+// const indexOfDot = fileName.indexOf(".");
+// const extension = fileName.slice(indexOfDot + 1)
+// console.log(extension);
+
+
+//function solution
+
+function getExtension(fileName) {
+    const indexOfDot = fileName.indexOf(".");
+    return fileName.slice(indexOfDot + 1);
+
+}
+console.log(getExtension
+    ('image.jpg'));
