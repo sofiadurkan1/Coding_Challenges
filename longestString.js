3e0bb4011c0f25e6e358e716d94495585784ed72
sofia durkan <sofiadurkan@gmail.com>
2021-07-19 22:04:37 -0500

    first commit

--- longestString.js
+++ longestString.js
@@ -1,13 +1,34 @@
 //Write a function that accepts an array of strings. Return the longest string.
 
-const sentence = ['Gurol', 'Havanur','Sofia','Michael'];
-let longestString = '';
-for (let i = 0; i <sentence.length; i++) {
-    if (sentence[i].length> longestString.length) {
-        longestString = sentence[i]
+// const sentence = ['Gurol', 'Havanur','Sofia','Michael'];
+// let longestString = '';
+// for (let i = 0; i <sentence.length; i++) {
+//     if (sentence[i].length> longestString.length) {
+//         longestString = sentence[i]
        
-}
+// }
 
 
-}
+// }
+// console.log(longestString);
+
+
+//forEach Method
+
+const myArray = ['Gurol', 'Havanur','Sofia','Michael'];
+let longestString = '';
+
+myArray.forEach(function(element){
+    if (element.length> longestString.length) {
+        longestString = element
+    }
+
+})
+    
 console.log(longestString);
+
+
+
+//sort Method
+// const myArray = ['Michael', 'Sofia', 'Havvanur'];
+// console.log(myArray.sort((a, b) => b.length - a.length)[0])
