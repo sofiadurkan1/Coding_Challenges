05467f3fdf833553eb74f09b8b1f58d4906acd8d
sofia durkan <sofiadurkan@gmail.com>
2021-07-23 07:00:38 -0500

    completed

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,14 +1,25 @@
 let input = prompt("What would you like to do?");
-const todos = ['collect eggs','clean']
-while (input !=='quit' && input !== 'q'){
-    if(input === 'list'){
-        console.log('*******************')
-        for (let i = 0; i < todos.length; i++){
-            
-
-        }
+const todos = ["collect eggs", "clean"];
+while (input !== "quit" && input !== "q") {
+  if (input === "list") {
+    console.log("*******************");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
     }
-    input = prompt("What would you like to do?")
-
+    console.log("*******************");
+  } else if (input === "new") {
+    const newTodo = prompt("ok, What is the new todo?");
+    todos.push(newTodo);
+    console.log(`${newTodo} added to the list!`);
+  } else if (input === "delete") {
+    const index = parseInt(prompt("Ok, enter an index to delete : "));
+    if (!Number.isNaN(index)) {
+      const deleted = todos.splice(index, 1);
+      console.log(`Ok,deleted ${deleted[0]}`);
+    } else {
+      console.log("Unknown Index");
+    }
+  }
+  input = prompt("What would you like to do?");
 }
-console.log('Ok! Quit the App')
+console.log("Ok! Quit the App");
