a5f71c64eaa37a30f3d8a9129d178baa4c18c0cb
sofia durkan <sofiadurkan@gmail.com>
2021-07-22 23:29:50 -0500

    first commit

---
+++ ToDoList/app.js
@@ -0,0 +1,14 @@
+let input = prompt("What would you like to do?");
+const todos = ['collect eggs','clean']
+while (input !=='quit' && input !== 'q'){
+    if(input === 'list'){
+        console.log('*******************')
+        for (let i = 0; i < todos.length; i++){
+            
+
+        }
+    }
+    input = prompt("What would you like to do?")
+
+}
+console.log('Ok! Quit the App')
