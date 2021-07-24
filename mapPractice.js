0c00a2d6962eb7fe998cad331bcc3620d107879f
sofia durkan <sofiadurkan@gmail.com>
2021-07-24 17:49:18 -0500

    completed

--- mapPractice.js
+++ mapPractice.js
@@ -4,7 +4,7 @@
 const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
 
 
-const names = fullNames.map(function(fName){
+const firstNames = fullNames.map(function(fName){
     return fName.first;
 })
 console.log(names);
