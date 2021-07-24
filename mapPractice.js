7d22f9d5ae7418966ba05bb0b855dc36323c9c64
sofia durkan <sofiadurkan@gmail.com>
2021-07-24 07:10:19 -0500

    first commit

---
+++ mapPractice.js
@@ -0,0 +1,10 @@
+//define a variable named firstNames and assign it to result of mapping over existing arrays.
+
+
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+
+const names = fullNames.map(function(fName){
+    return fName.first;
+})
+console.log(names);
