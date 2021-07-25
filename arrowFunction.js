a682f02e0e46d3a58de97fc83043ab49b8d26512
sofia durkan <sofiadurkan@gmail.com>
2021-07-24 22:06:24 -0500

    completed

---
+++ arrowFunction.js
@@ -0,0 +1,13 @@
+
+//write a arrow function called greet. it should accept a single string argument,representing a person s name.
+
+
+
+
+const greet = personName =>{
+    return `Hey ${personName}!`
+
+}
+
+console.log(greet("Mina"));
+console.log(greet("Beren"));
