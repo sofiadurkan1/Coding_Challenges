1e11b1da3afec1d46f9d801d22b88376b11897d8
sofia durkan <sofiadurkan@gmail.com>
2021-07-29 17:23:19 -0500

    first question

---
+++ HackerRank/helloWorld.js
@@ -0,0 +1,9 @@
+//write a function that prints Hello World!
+
+function writeHelloWorld(){
+    let myStr="hello world";
+    return myStr;
+    // return 'Hello World!';
+
+}
+console.log(writeHelloWorld());
